f6cdbd72fab3dac97655153e69294dfb5426d1c1
mandioquynha <machado.amandag@gmail.com>
2022-12-03 20:37:52 -0300

    feat: :sparkles: implementada função search_by_title
    requisito 6 concluido

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    data = search_news({"title": {"$regex": f"{title}", "$options": "i"}})
+    news = []
+    for item in data:
+        news.append((item["title"], item["url"]))
+    return news
 
 
 # Requisito 7
