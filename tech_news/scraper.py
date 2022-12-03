4c70ebfc06605145507c9c4d1aa5b95f6c3e734b
mandioquynha <machado.amandag@gmail.com>
2022-12-03 17:01:35 -0300

    feat: :sparkles: implementada função scrape_noticia
    requisito 4 concluido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,18 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    news = {
+        "url": selector.css("link[rel='canonical']::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css(".comment-list li").getall()),
+        "summary": "".join(selector.css(".entry-content > p:nth-of-type(1) *::text").getall()).strip(),
+        "tags": selector.css("section.post-tags ul li a::text").getall(),
+        "category": selector.css(".category-style .label::text").get(),
+    }
+    return news
 
 
 # Requisito 5
