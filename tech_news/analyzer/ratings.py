96eefe0ad5ca18780f58ea4fda6d93a0e7534eb3
mandioquynha <machado.amandag@gmail.com>
2022-12-04 11:54:25 -0300

    feat: :sparkles: implementada função top_5_news
    requisito 10 concluido

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    data = find_news()
+    news = []
+    create_return = sorted(
+        data,
+        key=lambda new: new["comments_count"],
+        reverse=True,
+    )
+    for item in create_return[:5]:
+        news.append((item["title"], item["url"]))
+    return news
+
 
 
 # Requisito 11
